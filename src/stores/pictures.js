7a35c0d96e7c20ed788cbad2c6205465e0ef55d9
Cyprien Malherbe <cmalherbe@systra.com>
2026-02-27 21:19:17 +0100

    add newspaper & Les Nymphéas Courcelles

--- src/stores/pictures.js
+++ src/stores/pictures.js
@@ -10,6 +10,11 @@ import presentationBistrotDuClocherImg from '@/assets/presentationBistrotDuCloch
 import shadyBuzzBistrotDuClocherImg from '@/assets/shadyBuzzBistrotDuClocher.jpeg'
 import giveBistrotDuClocherImg from '@/assets/give-bistrot-du-clocher.jpeg'
 import laughsBistrotDuClocherImg from '@/assets/laughs-bistrot-du-clocher.jpeg'
+import CourcellesAllAgain from '@/assets/Courcelles-all-again.png'
+import CourcellesAll from '@/assets/Courcelles-all.png'
+import CourcellesMe from '@/assets/Courcelles-me.png'
+import CourcellesSong from '@/assets/Courcelles-song.png'
+import NewsPaper from '@/assets/newspaper.jpeg'
 
 function slugify(text) {
   return text
@@ -82,6 +87,21 @@ export const usePicturesStore = defineStore('pictures', {
         subtitle3: "Une super ambiance qui à n'en point douter aura ravi tous les participants qui reviendront certainement 😊.",
         subtitle4: "Merci à Stéphane, à toute l'équipe du bistrot du clocher et à la mairie de Saint-Aubin-sur-Gaillon pour cette première opportunité",
         subtitle5: "N'hésitez pas à y faire un tour, l'ambiance vaut le détour.",
+      },
+      {
+        title: "Centre de loisirs 'Les Nymphéas'",
+        images: [CourcellesMe, CourcellesSong, CourcellesAll, CourcellesAllAgain],
+        published: new Date("2026-02-27T03:24:00"),
+        subtitle1: "Une première pour Shady Buzz dans un centre de loisirs, à Courcelles sur Seine.",
+        subtitle2: "Plusieurs animations avec des buzzers, des mimes, des jeux de pancarte qui ont ravi nos jeunes joueuses et joueurs (âgés de 7 à 10 ans) mais également nos moins jeunes mais tout autant enjouées animatrices.",
+        subtitle3: "Merci à toute l'équipe de m'avoir offert cette opportunité. Nous devrions normalement nous revoir incessamment, et ce sera avec plaisir 😊.",
+      },
+      {
+        title: "Shady Buzz dans le journal",
+        images: [NewsPaper],
+        published: new Date("2026-02-27T03:23:00"),
+        subtitle1: "Shady Buzz entre dans le patrimoine local, bon d'accord pas encore mais c'est un bon début pour une entreprise qui s'est lancée il y a à peine 1 mois. Une petite fierté quand même 😊.",
+        subtitle2: "Merci à Isabelle pour l'article 😊.",
       },
     ],
   }),
